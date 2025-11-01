d0ce837d055339d94b2c9bc657227c4c7f822d5f
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-11-01 14:24:00 +0300

    Trim whitespace from readContacts function data after reading from file

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 
 export const readContacts = async () => {
   try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const data = (await fs.readFile(PATH_DB, 'utf-8')).trim();
     if (!data) {
       console.log('No contacts found!');
       return [];
