633e9c29065a99995c29b3bf398ce2dbd5aa6bf8
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-11-01 16:03:09 +0300

    Implement removeLastContact function to delete the last contact and handle errors

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,22 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log('No contacts to remove.');
+      return;
+    }
+    contacts.pop();
+    await writeContacts(contacts);
+    console.log(
+      'Last contact removed successfully.\nTotal contacts left:',
+      contacts.length,
+    );
+  } catch (error) {
+    console.error('Error removing last contact:', error);
+  }
+};
 
 removeLastContact();
